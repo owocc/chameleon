8ff920a023ffabf83bc8772d3c858c6812a69cea
owocc <owocc@qq.com>
2024-09-09 18:29:32 +0800

    优化架构中

--- apps/electron/drizzle/0000_nebulous_nightcrawler.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE `color` (
-	`id` integer PRIMARY KEY AUTOINCREMENT NOT NULL,
-	`colors` text NOT NULL
-);
---> statement-breakpoint
-CREATE TABLE `config` (
-	`id` integer PRIMARY KEY DEFAULT 0 NOT NULL,
-	`them_mode` text DEFAULT 'auto'
-);
